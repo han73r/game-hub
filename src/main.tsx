c717eeea2a4398cb87e2aafe7aab6b89b6558141
Alex Dee <han73r@gmail.com>
2024-11-05 17:15:24 +0700

    Build a responsive layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
+import { Provider } from "@/components/ui/provider";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "./components/ui/provider";
 import App from "./App";
-import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider>
       <App />
