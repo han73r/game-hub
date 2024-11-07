02a51fbc60120ccd4c86cb26caf2a4276ee00878
Alex Dee <han73r@gmail.com>
2024-11-07 11:53:01 +0700

    Add Cards and responsive grid

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,15 +2,16 @@ import apiClient from "@/services/api-client";
 import { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 
-interface Game {
+export interface Game {
     id: number;
     name: string;
+    background_image: string;
   }
   
-  interface FetchGamesResponce {
-    count: number;
-    results: Game[];
-  }
+interface FetchGamesResponce {
+count: number;
+results: Game[];
+}
 
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
